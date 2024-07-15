51402df242bacb985b290ad65b3033a64da2b5b1
jieggii <jieggii@protonmail.com>
2024-07-15 19:00:47 +0300

    bot: update list command

--- services/bot/bot/handlers/list.py
+++ services/bot/bot/handlers/list.py
@@ -1,8 +1,11 @@
+import datetime
+
 from aiogram import Router
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
 from podcastie_database.models import Podcast, User
+from podcastie_telegram_html import link
 
 from bot.middlewares import DatabaseMiddleware
 
@@ -11,7 +14,7 @@ router.message.middleware(DatabaseMiddleware())
 
 
 @router.message(Command("list"))
-async def cancel_handler(message: Message, state: FSMContext, user: User) -> None:
+async def handle_list_command(message: Message, state: FSMContext, user: User) -> None:
     if not user.following_podcasts:
         await message.answer(
             "📭 You don't follow any podcasts yet! Start by using the /follow command."
@@ -21,11 +24,12 @@ async def cancel_handler(message: Message, state: FSMContext, user: User) -> Non
     response = "List of podcasts you follow:\n"
     for object_id in user.following_podcasts:
         podcast = await Podcast.find_one(Podcast.id == object_id)
-        fmt_title = (
-            f'<a href="{podcast.link}">{podcast.title}</a>'
-            if podcast.link
-            else podcast.title
+        fmt_status = "✅" if podcast.latest_episode_check_successful else "❌"
+        fmt_last_check_date = datetime.datetime.fromtimestamp(podcast.latest_episode_checked).strftime("%H:%M:%S %D.%M.%Y")
+        response += (
+            f"• {link(podcast.title, podcast.link)} "
+            f"<code>{podcast.ppid}</code> "
+            f"(last check: {fmt_last_check_date}, status: {fmt_status})\n\n"
         )
-        response += f"• {fmt_title} (<code>{podcast.ppid}</code>)\n"
 
     await message.answer(response, disable_web_page_preview=True)
