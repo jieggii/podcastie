ebd1b46af77ccea2d8b56504723d377bfae4a79b
jieggii <jieggii@protonmail.com>
2024-07-18 17:43:14 +0300

    bot: fix typo in `/faq` response

--- services/bot/bot/handlers/faq.py
+++ services/bot/bot/handlers/faq.py
@@ -12,7 +12,7 @@ async def handle_faq(message: Message) -> None:
         "<b>1. What is RSS feed and where do I get a URL for it?</b>\n"
         "<i>For podcasts, an RSS feed contains the latest episodes and information about the show."
         "You can usually find the RSS feed URL on the podcast's website. "
-        "Look for the RSS icon, typically orange and white, often found in the footer or on the subscription page."
+        "Look for the RSS icon, typically orange and white, often found in the footer or on the subscription page.</i>"
         "\n"
         "<b>2. What is PPID?</b>\n"
         "<i>PPID (Podcastie Podcast ID) is a unique identifier of a podcast within the Podcastie's database. "
