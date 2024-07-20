56331a1afdd7bce1b09ee49b15052ac066923ea4
jieggii <jieggii@protonmail.com>
2024-07-20 10:21:30 +0300

    bot: add `create_user` param to `DatabaseMiddleware`

--- services/bot/bot/middlewares.py
+++ services/bot/bot/middlewares.py
@@ -1,22 +1,24 @@
 from typing import Any, Awaitable, Callable, Dict
 
 from aiogram import BaseMiddleware
-from aiogram.types import Message
+from aiogram.types import Message, InlineQuery
 from podcastie_database import User
 
 
 class DatabaseMiddleware(BaseMiddleware):
-    def __init__(self) -> None:
-        pass
+    _create_user: bool
+
+    def __init__(self, create_user: bool = True) -> None:
+        self._create_user = create_user
 
     async def __call__(
         self,
-        handler: Callable[[Message, Dict[str, Any]], Awaitable[Any]],
-        event: Message,
+        handler: Callable[[Message | InlineQuery, Dict[str, Any]], Awaitable[Any]],
+        event: Message | InlineQuery,
         data: Dict[str, Any],
     ) -> Any:
         user = await User.find_one(User.user_id == event.from_user.id)
-        if not user:
+        if not user and self._create_user:
             user = User(user_id=event.from_user.id)
             await user.insert()
         data["user"] = user
