9bae09c97e66c26de79adce1b835cac285885e63
jieggii <jieggii@protonmail.com>
2024-07-20 14:22:46 +0300

    bot: basic minimal test inline query search

--- services/bot/bot/router/inline_query.py
+++ services/bot/bot/router/inline_query.py
@@ -1,8 +1,9 @@
 from aiogram import Router
 from aiogram.types import InlineQuery, InlineQueryResultArticle, InputTextMessageContent
 import hashlib
+from beanie.operators import Text
 
-from podcastie_database import User
+from podcastie_database import User, Podcast
 
 from bot.middlewares import DatabaseMiddleware
 
@@ -13,22 +14,22 @@ router.inline_query.middleware(DatabaseMiddleware(create_user=False))
 
 @router.inline_query()
 async def handle_inline_query(query: InlineQuery, user: User | None) -> None:
-    if user:
-        text = str(user.user_id)
-    else:
-        text = "unknown user"
+    query_text = query.query
+    # query_text_hash = hashlib.md5(query_text.encode()).hexdigest()
 
-    input_content = InputTextMessageContent(message_text=text)
-    result_id: str = hashlib.md5(text.encode()).hexdigest()
-    item = InlineQueryResultArticle(
-        id=result_id,
-        title=f'Result {text}',
-        input_message_content=input_content,
-    )
+    podcasts = await Podcast.find(Text(query_text)).to_list()
+    inline_results: list[InlineQueryResultArticle] = []
+    for podcast in podcasts:
+        input_message_content = InputTextMessageContent(message_text=podcast.link if podcast.link else podcast.ppid)  # todo
+        inline_results.append(InlineQueryResultArticle(
+            id=podcast.ppid,
+            title=podcast.title,
+            input_message_content=input_message_content,
+            description=podcast.description,
+            thumbnail_url=None  # todo
+        ))
 
     await query.answer(
-        results=[
-            item
-        ],
+        results=inline_results,
         cache_time=1
     )
