d396873ee04be603425c55ee295719876f559de1
jieggii <jieggii@protonmail.com>
2024-07-22 16:59:12 +0300

    bot implement basic podcast cards
    - description is not supported as some of them break telegram HTML parsing
    - /start payload made to follow commands by one link is not implemented
      yet

--- services/bot/bot/router/inline_query.py
+++ services/bot/bot/router/inline_query.py
@@ -1,9 +1,11 @@
 from aiogram import Router
+from aiogram.enums import ParseMode
 from aiogram.types import InlineQuery, InlineQueryResultArticle, InputTextMessageContent
 import hashlib
 from beanie.operators import Text
 
 from podcastie_database import User, Podcast
+from podcastie_telegram_html import link, bold, optional, footer
 
 from bot.middlewares import DatabaseMiddleware
 
@@ -15,21 +17,51 @@ router.inline_query.middleware(DatabaseMiddleware(create_user=False))
 @router.inline_query()
 async def handle_inline_query(query: InlineQuery, user: User | None) -> None:
     query_text = query.query
-    # query_text_hash = hashlib.md5(query_text.encode()).hexdigest()
 
-    podcasts = await Podcast.find(Text(query_text)).to_list()
-    inline_results: list[InlineQueryResultArticle] = []
+    podcasts: list[Podcast]
+    result_is_personal: bool
+    if query_text:
+        result_is_personal = False
+        podcasts = await Podcast.find(Text(query_text)).to_list()
+    else:
+        if user and user.following_podcasts:
+            result_is_personal = True
+            podcasts = [
+                await Podcast.find_one(Podcast.id == podcast_id)
+                for podcast_id in user.following_podcasts
+            ]
+        else:
+            result_is_personal = True
+            podcasts = []
+
+    for podcast in podcasts: podcast.description = "Description is not available yet."
+
+    articles: list[InlineQueryResultArticle] = []
     for podcast in podcasts:
-        input_message_content = InputTextMessageContent(message_text=podcast.link if podcast.link else podcast.ppid)  # todo
-        inline_results.append(InlineQueryResultArticle(
+        footer_items: list[str] = []
+        if podcast.link:
+            footer_items.append(link("podcast website", podcast.link))
+        footer_items.append(link("subscribe", f"https://t.me/podcastie_bot?start={podcast.ppid}"))
+
+        text = (
+            f"{bold(podcast.title)}\n\n"
+            f"{optional(podcast.description)}\n\n"
+            f"{footer(footer_items)}"
+        )
+        message_content = InputTextMessageContent(
+            message_text=text, parse_mode=ParseMode.HTML  # todo remove html
+        )
+
+        articles.append(InlineQueryResultArticle(
             id=podcast.ppid,
             title=podcast.title,
-            input_message_content=input_message_content,
+            input_message_content=message_content,
             description=podcast.description,
             thumbnail_url=podcast.cover_url
         ))
 
     await query.answer(
-        results=inline_results,
-        cache_time=1
+        results=articles,
+        cache_time=1,
+        is_personal=result_is_personal,
     )
