1882d4398888b61dd084c1e90b0474b8948f612b
jieggii <jieggii@protonmail.com>
2024-07-24 21:15:31 +0300

    bot: `/start` command: handle ppid provided as arg
    When user follows link of form "https://t.me/<bot>?start=<ppid>" bot
    receives this ppid as an argument (e.g. "/start <ppid>"). This ppid is
    parsed and used to subscribe to the given podcast

--- services/bot/bot/router/command/start.py
+++ services/bot/bot/router/command/start.py
@@ -1,19 +1,59 @@
+import base64
+
 from aiogram import Router
 from aiogram.filters import CommandStart
 from aiogram.types import Message
 
-router = Router()
+from podcastie_database import User
+from podcastie_telegram_html import tags
+from bot.middlewares import DatabaseMiddleware
+from bot.validators import is_ppid
+from bot.core.follow_transaction import FollowTransaction
 
+router = Router()
+router.message.middleware(DatabaseMiddleware(create_user=False))
 
 @router.message(CommandStart())
-async def handle_start(message: Message) -> None:
-    await message.answer(
-        f"👋 Hi there, {message.from_user.first_name}!\n"
-        f"\n"
-        "I'm <a href='https://t.me/podcastie_bot'>Podcastie Bot</a>, "
-        "and I'm here to help you stay updated with your favorite podcasts! 🎧\n"
-        "\n"
-        "To get started, simply grab the RSS feed URL of your favorite podcast and use the /follow command.\n"
-        "\n"
-        "Type /help to see all available commands.",
-    )
+async def handle_start(message: Message, user: User) -> None:
+    tokens = message.text.split(maxsplit=2)
+    ppid: str | None = None
+    if len(tokens) == 2:
+        ppid_encoded = tokens[1]
+        ppid = base64.urlsafe_b64decode(ppid_encoded.encode()).decode()
+
+    if not user or (user and not ppid):
+        await message.answer(
+            f"👋 Hi there, {message.from_user.first_name}!\n"
+            f"\n"
+            "I'm <a href='https://t.me/podcastie_bot'>Podcastie Bot</a>, "
+            "and I'm here to help you stay updated with your favorite podcasts! 🎧\n"
+            "\n"
+            "To get started, simply grab the RSS feed URL of your favorite podcast and use the /follow command.\n"
+            "\n"
+            "Type /help to see all available commands.",
+        )
+
+    if not ppid:
+        return
+
+    if not is_ppid(ppid):
+        await message.answer("Provided PPID is not valid.")  # todo
+        return
+
+    if not user:
+        user = User(user_id=message.from_user.id)  # todo: .from_user_id
+        await user.insert()
+
+    transaction = FollowTransaction(user)
+    await transaction.follow_podcast_by_ppid(ppid)
+    result = await transaction.commit()
+
+    text: str
+    if result.succeeded:
+        item = result.succeeded[0]
+        text = f"You have successfully subscribed to {tags.link(item.podcast_title, item.podcast_link)}"
+    else:
+        item = result.failed[0]
+        text = f"Failed to subscribe to {tags.link(item.podcast_title, item.podcast_link)}, reason: {item.message}"
+
+    await message.answer(text, disable_web_page_preview=len(result.succeeded) == 0)
