a6949ff5535755d8ec4f684e0fa9aafc53d8d3e3
jieggii <jieggii@protonmail.com>
2024-07-30 23:30:19 +0300

    podcastie_telegram_html: new `start_bot_link` component

--- lib/podcastie_telegram_html/podcastie_telegram_html/components.py
+++ lib/podcastie_telegram_html/podcastie_telegram_html/components.py
@@ -1,6 +1,15 @@
+from base64 import urlsafe_b64encode
+
+from . import tags
+
 def footer(items: list[str]) -> str:
     return " • ".join(items)
 
 
-def optional(text: str | None) -> str:
-    return text if text else ""
+def optional(child: str | None) -> str:
+    return child if child else ""
+
+def start_bot_link(child: str, bot_username: str, payload: str, encode_payload: bool = False) -> str:
+    if encode_payload:
+        payload = urlsafe_b64encode(payload.encode()).decode()
+    return tags.link(child, f"https://t.me/{bot_username}?start={payload}")
