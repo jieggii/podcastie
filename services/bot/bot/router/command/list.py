1d2182f4a333c3c89ad2bd36f46aa5abc9650571
jieggii <jieggii@protonmail.com>
2024-08-03 08:55:46 +0300

    bot: `/list` command: format code

--- services/bot/bot/router/command/list.py
+++ services/bot/bot/router/command/list.py
@@ -3,7 +3,7 @@ from aiogram.filters import Command
 from aiogram.types import Message
 from podcastie_database.models.podcast import Podcast
 from podcastie_database.models.user import User
-from podcastie_telegram_html.tags import link, code
+from podcastie_telegram_html.tags import code, link
 
 from bot.filters import StatePresenceFilter
 from bot.middlewares import DatabaseMiddleware
@@ -26,8 +26,6 @@ async def handle_list_command(message: Message, user: User) -> None:
 
         status = "👌" if podcast.latest_episode_info.check_success else "⚠️"
 
-        response += (
-            f"{status} {link(podcast.meta.title, podcast.meta.link)} ({code(podcast.ppid)})\n\n"
-        )
+        response += f"{status} {link(podcast.meta.title, podcast.meta.link)} ({code(podcast.ppid)})\n\n"
 
     await message.answer(response, disable_web_page_preview=True)
