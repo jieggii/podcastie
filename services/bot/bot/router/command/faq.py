f600ae42a58e5a2bcfd8d925cb9250e39c0b9e32
jieggii <jieggii@protonmail.com>
2024-08-03 09:18:58 +0300

    bot: `/faq` command: update response command

--- services/bot/bot/router/command/faq.py
+++ services/bot/bot/router/command/faq.py
@@ -2,21 +2,40 @@ from aiogram import Router
 from aiogram.filters import Command
 from aiogram.types import Message
 
+from podcastie_telegram_html.tags import bold, italic
+
 from bot.filters import StatePresenceFilter
 
 router = Router()
 
 
+_QUESTIONS = {
+    "What is RSS feed?": (
+        "An RSS feed is a special link that lets you get the latest episodes and updates from a podcast automatically. "
+        "Think of it as a subscription service that keeps you up-to-date with new content without having to check manually. "
+        "Here is an example RSS feed URL to the Joe Rogan Experience podcast: https://feeds.megaphone.fm/GLT1412515089."
+    ),
+    "Where do I get my favorite podcast RSS feed URL?": (
+        "You can usually find the RSS feed URL on the podcast’s official website. "
+        "Look for an icon that looks like a small orange radio wave, often found at the bottom of the page or on a “Subscribe” section. "
+        'If you have trouble, you can often ask the podcast host or search online for "[Podcast Name] RSS".'
+    ),
+    "What is PPID?": (
+        "PPID (Podcastie Podcast ID) is a unique identifier of a podcast withing the Podcastie's database. "
+        "This ID helps you manage and interact with podcasts easily."
+    )
+}
+
+_MESSAGE = ""
+for i, (q, a) in enumerate(_QUESTIONS.items()):
+    _MESSAGE += (
+        f"{bold(f"{i + 1}. {q}")}\n"
+        f"{italic(a)}\n"
+        "\n"
+    )
+
+
 @router.message(Command("faq"), StatePresenceFilter(has_state=False))
 async def handle_faq(message: Message) -> None:
     await message.answer("Here are some useful questions and answers to them!")
-    await message.answer(
-        "<b>1. What is RSS feed and where do I get a URL for it?</b>\n"
-        "<i>For podcasts, an RSS feed contains the latest episodes and information about the show."
-        "You can usually find the RSS feed URL on the podcast's website. "
-        "Look for the RSS icon, typically orange and white, often found in the footer or on the subscription page.</i>\n"
-        "\n"
-        "<b>2. What is PPID?</b>\n"
-        "<i>PPID (Podcastie Podcast ID) is a unique identifier of a podcast within the Podcastie's database. "
-        "This ID helps you manage and interact with podcasts easily.</i>"
-    )
+    await message.answer(_MESSAGE, disable_web_page_preview=True)
