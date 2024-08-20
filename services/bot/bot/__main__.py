6cc360d3ae94a103c1db5e186fb44a093e965dbf
jieggii <jieggii@protonmail.com>
2024-08-20 23:04:03 +0300

    WIP: continue cleaning up

--- services/bot/bot/__main__.py
+++ services/bot/bot/__main__.py
@@ -4,7 +4,7 @@ import sys
 
 import aiogram.loggers
 import structlog
-from aiogram import Bot, Dispatcher, Router
+from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from podcastie_database.init import init_database
@@ -33,25 +33,6 @@ from bot.views.start_view import StartView
 from bot.views.subscriptions_view import SubscriptionsView
 from bot.views.unfollow_view import UnfollowView
 
-# from bot.router import inline_query
-# from bot.router.callback import (
-#     menu as menu_callback,
-#     find as find_callback,
-#     follow as follow_callback,
-#     unfollow as unfollow_callback,
-#     import_ as import_callback,
-#     subscriptions as subscriptions_callback,
-#     podcast as podcast_callback,
-# )
-# from bot.router.command import (
-#     start as start_command,
-#     menu as menu_command
-# )
-# from bot.router.state import (
-#     find as find_state,
-#     import_ as import_state,
-# )
-
 
 def setup_logging():
     logging.getLogger("pymongo").setLevel(logging.WARNING)
@@ -60,16 +41,16 @@ def setup_logging():
     logging.getLogger("aiogram.webhook").setLevel(logging.WARNING)
 
     aiogram.loggers.middlewares = structlog.get_logger()
-    logging.getLogger("aiogram.middlewares").setLevel(logging.DEBUG)
+    logging.getLogger("aiogram.middlewares").setLevel(logging.WARNING)
 
     aiogram.loggers.event = structlog.get_logger()
-    logging.getLogger("aiogram.event").setLevel(logging.DEBUG)
+    logging.getLogger("aiogram.event").setLevel(logging.WARNING)
 
     aiogram.loggers.dispatcher = structlog.get_logger()
-    logging.getLogger("aiogram.dispatcher").setLevel(logging.DEBUG)
+    logging.getLogger("aiogram.dispatcher").setLevel(logging.WARNING)
 
     aiogram.loggers.scene = structlog.get_logger()
-    logging.getLogger("aiogram.scene").setLevel(logging.DEBUG)
+    logging.getLogger("aiogram.scene").setLevel(logging.WARNING)
 
 
 async def main() -> None:
@@ -104,45 +85,45 @@ async def main() -> None:
         ),
         ViewRouter(
             MenuView(),
-            MenuViewEntrypointCallbackData,
+            entrypoint_callback_data_type=MenuViewEntrypointCallbackData,
             entrypoint_command="menu",
             entrypoint_handler_middlewares=[DatabaseMiddleware()],
         ),
         ViewRouter(
             FindView(),
-            FindViewEntrypointCallbackData,
+            entrypoint_callback_data_type=FindViewEntrypointCallbackData,
             handle_state=BotState.FIND,
             state_handler_middlewares=[DatabaseMiddleware()],
         ),
         ViewRouter(
             ImportView(),
-            ImportViewEntrypointCallbackData,
+            entrypoint_callback_data_type=ImportViewEntrypointCallbackData,
             handle_state=BotState.IMPORT,
             state_handler_middlewares=[DatabaseMiddleware()],
         ),
         ViewRouter(
             ExportView(),
-            ExportViewEntrypointCallbackData,
+            entrypoint_callback_data_type=ExportViewEntrypointCallbackData,
             entrypoint_handler_middlewares=[DatabaseMiddleware()],
         ),
         ViewRouter(
             SubscriptionsView(),
-            SubscriptionsViewEntrypointCallbackData,
+            entrypoint_callback_data_type=SubscriptionsViewEntrypointCallbackData,
             entrypoint_handler_middlewares=[DatabaseMiddleware()],
         ),
         ViewRouter(
             PodcastView(),
-            PodcastViewEntrypointCallbackData,
+            entrypoint_callback_data_type=PodcastViewEntrypointCallbackData,
             entrypoint_handler_middlewares=[DatabaseMiddleware()],
         ),
         ViewRouter(
             ShareView(),
-            ShareViewEntrypointCallbackData,
+            entrypoint_callback_data_type=ShareViewEntrypointCallbackData,
             entrypoint_handler_middlewares=[DatabaseMiddleware()],
         ),
         ViewRouter(
             UnfollowView(),
-            UnfollowViewEntrypointCallbackData,
+            entrypoint_callback_data_type=UnfollowViewEntrypointCallbackData,
             entrypoint_handler_middlewares=[DatabaseMiddleware()],
         ),
     )
@@ -150,7 +131,10 @@ async def main() -> None:
     # create and start bot:
     bot = Bot(
         token=env.TelegramBot.TOKEN,
-        default=DefaultBotProperties(parse_mode=ParseMode.HTML),
+        default=DefaultBotProperties(
+            parse_mode=ParseMode.HTML,
+            link_preview_is_disabled=True
+        ),
     )
     dp["bot"] = bot
 
