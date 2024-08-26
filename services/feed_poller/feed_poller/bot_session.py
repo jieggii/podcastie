238963c811c0a4f102094432d1aa77526d9de01e
jieggii <jieggii@protonmail.com>
2024-08-26 22:53:09 +0300

    docker-compose.yml: small fix in ports

--- services/feed_poller/feed_poller/bot_session.py
+++
@@ -1,8 +0,0 @@
-from aiogram.client.session.aiohttp import AiohttpSession
-from aiogram.client.telegram import TelegramAPIServer
-
-
-class LocalTelegramAPIAiohttpSession(AiohttpSession):
-    def __init__(self, base_url: str):
-        super().__init__()
-        self.api = TelegramAPIServer.from_base(base_url)
